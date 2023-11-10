7f91052372db1b0884961d1cd209119b6a86a1ea
Moamal6161 <100538652+Moamal6161@users.noreply.github.com>
2023-11-10 18:05:21 +0300

    Add files via upload

---
+++ MyAcounts.js
@@ -0,0 +1,7 @@
+function DWF(){
+    document.getElementById("DW").innerHTML = "muamal313";
+    document.getElementById("DW").style.fontSize = "20px";
+    document.getElementById("DW").style.position = "absolute;";
+    document.getElementById("DW").style.left = "72%";
+    document.getElementById("DW").style.top = "185px";
+}
