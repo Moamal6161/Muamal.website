9ca1f9b535004f37b2c852349fbfa0e5627cc140
Moamal6161 <100538652+Moamal6161@users.noreply.github.com>
2024-06-08 00:22:23 +0300

    Delete MyAcounts.js

--- MyAcounts.js
+++
@@ -1,7 +0,0 @@
-function DWF(){
-    document.getElementById("DW").innerHTML = "muamal313";
-    document.getElementById("DW").style.fontSize = "20px";
-    document.getElementById("DW").style.position = "absolute;";
-    document.getElementById("DW").style.left = "72%";
-    document.getElementById("DW").style.top = "185px";
-}
